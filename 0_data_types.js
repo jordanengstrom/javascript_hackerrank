60f0dce37c5a5e013c6a47a6b3e4a090a7a6e20c
jordanengstrom <jordan.engstrom@gmail.com>
2021-02-15 22:48:57 -0700

    learned about Number.parseInt() and Number.parseFloat() methods

---
+++ 0_data_types.js
@@ -0,0 +1,31 @@
+// const prompt = require('prompt-sync')();
+// const name = prompt('What is your name? ');
+// console.log(`Hey there ${name}`);
+
+
+const secondInteger = '5';
+const secondDecimal = '5.0';
+const secondString = 'a word';
+
+
+function performOperation(secondInteger, secondDecimal, secondString) {
+    // Declare a variable named 'firstInteger' and initialize with integer value 4.
+    const firstInteger = 4;
+    
+    // Declare a variable named 'firstDecimal' and initialize with floating-point value 4.0.
+    const firstDecimal = 4.0;
+    
+    // Declare a variable named 'firstString' and initialize with the string "HackerRank".
+    const firstString = 'HackerRank ';
+    
+    // Write code that uses console.log to print the sum of the 'firstInteger' and 'secondInteger' (converted to a Number type) on a new line.
+    console.log(Number.parseInt(firstInteger, 10) + Number.parseInt(secondInteger, 10));
+    
+    // Write code that uses console.log to print the sum of 'firstDecimal' and 'secondDecimal' (converted to a Number type) on a new line.
+    console.log(Number.parseFloat(firstDecimal) + Number.parseFloat(secondDecimal));
+    
+    // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The variable 'firstString' must be printed first.
+    console.log(firstString + secondString);
+}
+
+performOperation(secondInteger, secondDecimal, secondString)
